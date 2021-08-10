74132a1fec7afb199aa7af2acdee8ca107445d1b
Yuki Nishijima <yuki24@hey.com>
2021-08-09 23:24:14 -0400

    Fixes the icon loading issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,13 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import "@shoelace-style/shoelace"
+import { setBasePath } from '@shoelace-style/shoelace'
 import "@shoelace-style/shoelace/dist/themes/base.css"
 // import * as ShoelaceRails from "shoelace-rails"
 
 Rails.start()
 Turbolinks.start()
+
 // ShoelaceRails.start({ turbolinksInstance: Turbolinks })
+const rootUrl = document.currentScript.src.replace(/\/packs.*$/, '')
+setBasePath(rootUrl + '/packs/js/')
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,20 @@
 const { environment } = require('@rails/webpacker')
 
+// Shoelace config
+const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin')
+
+// Add shoelace assets to webpack's build process
+environment.plugins.append(
+  'CopyPlugin',
+  new CopyPlugin({
+    patterns: [
+      {
+        from: path.resolve(__dirname, '../../node_modules/@shoelace-style/shoelace/dist/assets'),
+        to: path.resolve(__dirname, '../../public/packs/js/assets')
+      }
+    ]
+  })
+)
+
 module.exports = environment
